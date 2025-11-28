b820cee4fed1314b297206f08df8abdc961ffad7
Shiny Lee <shinish512@gmail.com>
2025-11-28 22:00:22 +0900

    스플래시 화면 생성

---
+++ lib/splash.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+// [이재은] 스플래시 로띠 화면
+// 4초 길이
+// -> 추후에 라우팅 필요함 현재는 아직 라우팅은 안 되어있음
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _initialize();
+  }
+
+  Future<void> _initialize() async {
+    await Future.delayed(const Duration(seconds: 4));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffEEF5FA),
+      body: Center(
+        child: Lottie.asset(
+          'assets/animations/splash.json',
+          width: 300,
+          height: 300,
+          fit: BoxFit.contain,
+          repeat: true,
+        ),
+      ),
+    );
+  }
+}
