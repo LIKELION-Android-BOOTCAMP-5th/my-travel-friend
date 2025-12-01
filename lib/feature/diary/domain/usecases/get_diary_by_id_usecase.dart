43678945dc418f3633bc304dd8cdbc07d9e83ec6
Shiny Lee <shinish512@gmail.com>
2025-12-01 13:24:58 +0900

    다이어리 상세조회 usecase

---
+++ lib/feature/diary/domain/usecases/get_diary_by_id_usecase.dart
@@ -0,0 +1,13 @@
+import '../entities/diary_entity.dart';
+import '../repositories/diary_repository.dart';
+
+class GetDiaryByIdUseCase {
+  final DiaryRepository _diaryRepository;
+
+  GetDiaryByIdUseCase(this._diaryRepository);
+
+  // 다이어리 상세 조회(아이디로 가져오기)
+  Future<DiaryEntity> call(String id) {
+    return _diaryRepository.getDiaryById(id);
+  }
+}
