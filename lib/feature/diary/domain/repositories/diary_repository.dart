420f91bb93d057221a957b6cc41c978dcbf3381c
Shiny Lee <shinish512@gmail.com>
2025-12-01 13:12:27 +0900

    다이어리 목록 조회(가져오기) 수정

--- lib/feature/diary/domain/repositories/diary_repository.dart
+++ lib/feature/diary/domain/repositories/diary_repository.dart
@@ -2,13 +2,9 @@
 import '../entities/diary_entity.dart';
 
 abstract class DiaryRepository {
-  // 공유 다이어리 목록 조회(가져오기)
-  // -> 여행 아이디로 다이어리 조회, 단 비공개 처리된 게시물은 가져오지 않음
+  // 다이어리 목록 조회(가져오기)
+  // -> 여행 아이디로 다이어리 조회
   Future<List<DiaryEntity>> getOurDiaries(String tripId);
-
-  // 내 다이어리 목록 조회 (가져오기)
-  // -> 여행 아이디, 내 아이디로 조회, 비공개 처리 게시물 포함
-  Future<List<DiaryEntity>> getMyDiaries(String tripId, String userId);
 
   // 다이어리 상세 조회 (가져오기)
   // -> 다이어리 아이디로 조회
