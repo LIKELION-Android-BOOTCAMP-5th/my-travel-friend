88ca4709bb27a4db69f2e26af3b2421653dc2b51
Shiny Lee <shinish512@gmail.com>
2025-12-02 14:56:07 +0900

    로그인 버튼(구글, 카카오) 생성 및 로그인 화면 UI 수정

---
+++ lib/feature/auth/presentation/widgets/google_login_button.dart
@@ -0,0 +1,45 @@
+// [이재은] 구글 로그인 버튼 위젯 생성
+// 구글 디자인 가이드 규칙에 따라 생성함
+
+import 'package:flutter/material.dart';
+
+import '../../../../theme/app_font.dart';
+
+class GoogleLoginButton extends StatelessWidget {
+  final VoidCallback? onTap;
+  const GoogleLoginButton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.7,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Color(0xffFFFFFF),
+          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/google_logo_24.png'),
+              SizedBox(width: 16.0),
+              Text(
+                "구글로 시작하기",
+                textAlign: TextAlign.center,
+                style: AppFont.regularBold.copyWith(
+                  color: Color(0xff1F1F1F),
+                  decoration: TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
