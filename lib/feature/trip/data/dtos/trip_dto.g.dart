38540d74073705c3f4a5964b4418e20bf5ec46b0
ShinGH <rkdgus1005@naver.com>
2025-12-02 17:25:12 +0900

    여행 엔티티, 디티오 완성했습니다

---
+++ lib/feature/trip/data/dtos/trip_dto.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'trip_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_TripDto _$TripDtoFromJson(Map<String, dynamic> json) => _TripDto(
+  id: (json['id'] as num?)?.toInt(),
+  createdAt: json['created_at'] as String?,
+  title: json['title'] as String,
+  place: json['place'] as String,
+  startAt: json['start_at'] as String,
+  endAt: json['end_at'] as String,
+  coverType: json['cover_type'] as String,
+  coverImg: json['cover_img'] as String?,
+  userId: (json['user_id'] as num).toInt(),
+  deletedAt: json['deleted_at'] as String?,
+  country: json['country'] as String,
+);
+
+Map<String, dynamic> _$TripDtoToJson(_TripDto instance) => <String, dynamic>{
+  'id': instance.id,
+  'created_at': instance.createdAt,
+  'title': instance.title,
+  'place': instance.place,
+  'start_at': instance.startAt,
+  'end_at': instance.endAt,
+  'cover_type': instance.coverType,
+  'cover_img': instance.coverImg,
+  'user_id': instance.userId,
+  'deleted_at': instance.deletedAt,
+  'country': instance.country,
+};
