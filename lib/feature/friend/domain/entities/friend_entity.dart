30bdc2829f1c9b2f2785c3828be05ff6ba0188cd
EomSB <pejesw236@gmail.com>
2025-12-02 11:35:25 +0900

    feat/friend entity 생성

---
+++ lib/feature/friend/domain/entities/friend_entity.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'friend_entity.freezed.dart';
+part 'friend_entity.g.dart';
+
+// [엄수빈] 프렌즈 엔티티 생성
+@freezed
+abstract class FriendEntity with _$FriendEntity {
+  const factory FriendEntity({
+    required String id, // 친구 관계 id
+    String? user1Id, // 친구 1
+    String? user2Id, // 친구 2
+    DateTime? createdAt, // 친구가 된 시점
+  }) = _FriendEntity;
+
+  factory FriendEntity.fromJson(Map<String, dynamic> json) =>
+      _$FriendEntityFromJson(json);
+}
