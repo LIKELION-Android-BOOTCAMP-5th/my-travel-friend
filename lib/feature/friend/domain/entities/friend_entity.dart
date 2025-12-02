6338cee86280243a2ac8460548616f87e06b89dc
EomSB <pejesw236@gmail.com>
2025-12-02 14:06:44 +0900

    feat/friend_request_entity 생성

--- lib/feature/friend/domain/entities/friend_entity.dart
+++ lib/feature/friend/domain/entities/friend_entity.dart
@@ -2,13 +2,13 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'friend_entity.freezed.dart';
 
-// [엄수빈] 프렌즈 엔티티 생성
+// [엄수빈] 친구 엔티티 생성
 @freezed
 abstract class FriendEntity with _$FriendEntity {
   const factory FriendEntity({
-    required String id, // 친구 관계 id
-    String? user1Id, // 친구 1
-    String? user2Id, // 친구 2
-    DateTime? createdAt, // 친구가 된 시점
+    required int id, // 친구 관계 id
+    int? user1Id, // 친구 1
+    int? user2Id, // 친구 2
+    String? createdAt, // 친구가 된 시점
   }) = _FriendEntity;
 }
---
+++ lib/feature/friend/domain/entities/friend_request_entity.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'friend_request_entity.freezed.dart';
+
+// [엄수빈] 친구 요청 엔티티 생성
+@freezed
+abstract class FriendRequestEntity with _$FriendRequestEntity {
+  const factory FriendRequestEntity({
+    required int id, // 친구 관계 id
+    int? requestId, // 요청한 친구
+    int? targetId, // 받은 친구
+    String? answeredAt, // 친구 수락 시점
+    String? createdAt, // 친구 요청한 시점
+  }) = _FriendRequestEntity;
+}
