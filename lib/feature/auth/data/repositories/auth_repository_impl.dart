3c796a9fb6b1592ec670444dcadd637bab06811f
Shiny Lee <shinish512@gmail.com>
2025-12-05 11:33:49 +0900

    애플 로그인 관련 authRepositoryImpl 수정

--- lib/feature/auth/data/repositories/auth_repository_impl.dart
+++ lib/feature/auth/data/repositories/auth_repository_impl.dart
@@ -1,20 +1,25 @@
 import 'package:injectable/injectable.dart';
 import 'package:my_travel_friend/core/result/failures.dart';
-import 'package:my_travel_friend/feature/auth/data/models/user_model.dart';
 import 'package:my_travel_friend/feature/auth/domain/entities/google_token_entity.dart';
 import 'package:my_travel_friend/feature/auth/domain/entities/user_entity.dart';
 
 import '../../../../core/result/result.dart';
 import '../../domain/repositories/auth_repository.dart';
+import '../datasources/apple_auth_data_source.dart';
 import '../datasources/google_auth_data_source.dart';
 import '../datasources/supabase_auth_data_source.dart';
 
 @Singleton(as: AuthRepository)
 class AuthRepositoryImpl implements AuthRepository {
   final GoogleAuthDataSource googleDataSource;
+  final AppleAuthDataSource appleDataSource; // [이재은] 애플 추가
   final SupabaseAuthDataSource supabaseAuthDataSource;
 
-  AuthRepositoryImpl(this.googleDataSource, this.supabaseAuthDataSource) {
+  AuthRepositoryImpl(
+    this.googleDataSource,
+    this.appleDataSource,
+    this.supabaseAuthDataSource,
+  ) {
     //슈퍼베이스 인증 리스너 구독 시작
     supabaseAuthDataSource.initializeAuthListener();
   }
@@ -52,9 +57,24 @@ class AuthRepositoryImpl implements AuthRepository {
       case SocialLoginType.kakao:
         // TODO: Handle this case.
         throw UnimplementedError();
-      case SocialLoginType.apple:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+      case SocialLoginType.apple: //[이재은] 애플 로그인 추가
+        final res = await appleDataSource.getAppleToken();
+        return res.when(
+          failure: (fail) =>
+              Result.failure(Failure.undefined(message: fail.message)),
+          success: (token) async {
+            final authResult = await supabaseAuthDataSource.signInWithToken(
+              type: type,
+              idToken: token.idToken,
+              accessToken: '', // Apple은 access token 불필요함
+            );
+
+            return authResult.when(
+              success: (user) => Result.success(user.toEntity()),
+              failure: (fail) => Result.failure(fail),
+            );
+          },
+        );
       case SocialLoginType.naver:
         // TODO: Handle this case.
         throw UnimplementedError();
