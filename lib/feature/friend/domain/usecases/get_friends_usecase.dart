dfee0df8edec08f8cc14086b4c6411e22c9fb7c1
EomSB <pejesw236@gmail.com>
2025-12-08 13:30:55 +0900

    친구 usecase

--- lib/feature/friend/domain/usecases/get_friends_usecase.dart
+++ lib/feature/friend/domain/usecases/get_friends_usecase.dart
@@ -1,15 +1,18 @@
+import 'package:injectable/injectable.dart';
+
+import '../../../../core/result/result.dart';
 import '../entities/friend_entity.dart';
 import '../repositories/friend_repository.dart';
 
 // [엄수빈] 친구 명단 가져오기 usecase
-// @LazySingleton()
+@LazySingleton()
 class GetFriendsUsecase {
   final FriendRepository _friendRepository;
 
   GetFriendsUsecase(this._friendRepository);
 
   // 다이어리 목록 가져오기
-  Future<List<FriendEntity>> call(int userId) {
+  Future<Result<List<FriendEntity>>> call(int userId) {
     return _friendRepository.getFriends(userId);
   }
 }
