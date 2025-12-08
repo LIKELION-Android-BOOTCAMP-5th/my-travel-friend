6964f6a7a51881c59ef54a645ecebac53c86fdb4
EomSB <pejesw236@gmail.com>
2025-12-08 14:23:47 +0900

    친구 요청 데이터구현 DI 수정

--- lib/feature/friend/data/datasources/friend_request_data_source_impl.dart
+++ lib/feature/friend/data/datasources/friend_request_data_source_impl.dart
@@ -9,15 +9,15 @@ import 'friend_request_data_source.dart';
 // [엄수빈] 친구 요청 데이터 소스 구현
 @LazySingleton(as: FriendRequestDataSource)
 class FriendRequestDataSourceImpl implements FriendRequestDataSource {
-  FriendRequestDataSourceImpl();
+  final SupabaseClient _supabaseClient;
+  FriendRequestDataSourceImpl(this._supabaseClient);
 
-  final supabase = Supabase.instance.client;
   static const _table = 'friend_request';
 
   @override
   Future<Result<List<FriendRequestDTO>>> getFriendRequest(int userId) async {
     try {
-      final response = await supabase
+      final response = await _supabaseClient
           .from(_table)
           .select()
           .eq('target_id', userId)
@@ -39,7 +39,7 @@ class FriendRequestDataSourceImpl implements FriendRequestDataSource {
     int targetId,
   ) async {
     try {
-      final response = await supabase
+      final response = await _supabaseClient
           .from(_table)
           .insert({'request_id': requestId, 'target_id': targetId})
           .select()
@@ -58,7 +58,10 @@ class FriendRequestDataSourceImpl implements FriendRequestDataSource {
     try {
       final now = DateTime.now().toIso8601String();
 
-      await supabase.from(_table).update({'answered_at': now}).eq('id', id);
+      await _supabaseClient
+          .from(_table)
+          .update({'answered_at': now})
+          .eq('id', id);
 
       return const Result.success(null);
     } catch (e) {
@@ -69,7 +72,7 @@ class FriendRequestDataSourceImpl implements FriendRequestDataSource {
   @override
   Future<Result<void>> deleteRequest(int id) async {
     try {
-      await supabase.from(_table).delete().eq('id', id);
+      await _supabaseClient.from(_table).delete().eq('id', id);
 
       return const Result.success(null);
     } catch (e) {
