98efec7bc4526d21baad75d7214491ff29086289
EomSB <pejesw236@gmail.com>
2025-12-08 13:30:55 +0900

    친구 usecase

--- lib/feature/friend/domain/usecases/accept_request_usecase.dart
+++ lib/feature/friend/domain/usecases/accept_request_usecase.dart
@@ -1,14 +1,17 @@
+import 'package:injectable/injectable.dart';
+
+import '../../../../core/result/result.dart';
 import '../repositories/friend_request_repository.dart';
 
 // [엄수빈] 친구 요청 수락 usecase
-// @LazySingleton()
+@LazySingleton()
 class AcceptRequestUsecase {
   final FriendRequestRepository _friendRequestRepository;
 
   AcceptRequestUsecase(this._friendRequestRepository);
 
   // 친구요청 수락
-  Future<void> call(int id) {
+  Future<Result<void>> call(int id) {
     return _friendRequestRepository.acceptRequest(id);
   }
 }
