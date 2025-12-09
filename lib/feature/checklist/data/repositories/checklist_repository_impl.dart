5b02d3d8043ead3ce07aa32019cfae7af7c77e32
Shiny Lee <shinish512@gmail.com>
2025-12-09 17:36:51 +0900

    체크리스트, 투두리스트 data repository

---
+++ lib/feature/checklist/data/repositories/checklist_repository_impl.dart
@@ -0,0 +1,51 @@
+import 'package:injectable/injectable.dart';
+import 'package:my_travel_friend/core/result/result.dart';
+
+import '../../domain/entities/checklist_entity.dart';
+import '../../domain/repositories/checklist_repository.dart';
+import '../datasources/checklist_data_source.dart';
+import '../dtos/checklist_dto.dart';
+
+// [이재은] 체크리스트 repository
+@LazySingleton(as: ChecklistRepository)
+class ChecklistRepositoryImpl implements ChecklistRepository {
+  final ChecklistDataSource _dataSource;
+  ChecklistRepositoryImpl(this._dataSource);
+
+  // 나의 체크리스트 가져오기
+  @override
+  Future<Result<List<ChecklistEntity>>> getMyChecklists(
+    int tripId,
+    int userId,
+  ) async {
+    final res = await _dataSource.getMyChecklists(
+      tripId: tripId,
+      userId: userId,
+    );
+    return res.when(
+      success: (data) =>
+          Result.success(data.map((dto) => dto.toEntity()).toList()),
+      failure: (failure) => Result.failure(failure),
+    );
+  }
+
+  // 체크리스트 생성
+  @override
+  Future<Result<ChecklistEntity>> createChecklist(
+    ChecklistEntity checklist,
+  ) async {
+    final dto = ChecklistDTO.fromEntity(checklist);
+
+    final res = await _dataSource.createChecklist(dto);
+    return res.when(
+      success: (data) => Result.success(data.toEntity()),
+      failure: (failure) => Result.failure(failure),
+    );
+  }
+
+  // 체크리스트 삭제
+  @override
+  Future<Result<void>> deleteChecklist(int id) async {
+    return await _dataSource.deleteChecklist(id);
+  }
+}
---
+++ lib/feature/checklist/data/repositories/todo_list_repository_impl.dart
@@ -0,0 +1,49 @@
+import 'package:injectable/injectable.dart';
+import 'package:my_travel_friend/core/result/result.dart';
+
+import '../../domain/entities/todo_list_entity.dart';
+import '../../domain/repositories/todo_list_repository.dart';
+import '../datasources/todo_list_data_source.dart';
+import '../dtos/todo_list_dto.dart';
+
+@LazySingleton(as: TodoListRepository)
+class TodoListRepositoryImpl implements TodoListRepository {
+  final TodoListDataSource _dataSource;
+
+  TodoListRepositoryImpl(this._dataSource);
+
+  // 나의 투두리스트 가져오기
+  @override
+  Future<Result<List<TodoListEntity>>> getMyTodoLists(
+    int tripId,
+    int userId,
+  ) async {
+    final res = await _dataSource.getMyTodoLists(
+      tripId: tripId,
+      userId: userId,
+    );
+    return res.when(
+      success: (data) =>
+          Result.success(data.map((dto) => dto.toEntity()).toList()),
+      failure: (failure) => Result.failure(failure),
+    );
+  }
+
+  // 투두리스트 생성
+  @override
+  Future<Result<TodoListEntity>> createTodoList(TodoListEntity todolist) async {
+    final dto = TodoListDTO.fromEntity(todolist);
+
+    final res = await _dataSource.createTodoList(dto);
+    return res.when(
+      success: (data) => Result.success(data.toEntity()),
+      failure: (failure) => Result.failure(failure),
+    );
+  }
+
+  // 투두리스트 삭제
+  @override
+  Future<Result<void>> deleteTodoList(int id) async {
+    return await _dataSource.deleteTodoList(id);
+  }
+}
