b801422daa71da77b58bd15c17d96ee880d79371
Shiny Lee <shinish512@gmail.com>
2025-12-09 22:08:02 +0900

    위젯 생성

---
+++ lib/feature/checklist/presentation/widgets/check_todo_tab.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../../../theme/app_font.dart';
+
+// [이재은] 체크리스트 <-> 투두리스트 탭 버튼
+class CheckTodoTab extends StatelessWidget {
+  final String label;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const CheckTodoTab({
+    super.key,
+    required this.label,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final isDark = colorScheme.brightness == Brightness.dark;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        decoration: BoxDecoration(
+          color: isSelected
+              ? colorScheme.primary
+              : colorScheme.surfaceContainerHighest,
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              label,
+              style: AppFont.regular.copyWith(
+                color: isSelected
+                    ? colorScheme.onSurface
+                    : colorScheme.onSurfaceVariant,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
