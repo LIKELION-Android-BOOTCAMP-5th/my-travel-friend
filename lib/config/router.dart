78f80a20deeed8209eb8214fe096b454adf85e30
Shiny Lee <shinish512@gmail.com>
2025-12-11 15:33:50 +0900

    profile 라우팅

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -22,6 +22,7 @@ import '../feature/diary/presentation/screens/new_diary/new_diary_bloc_widget.da
 import '../feature/diary/presentation/viewmodels/diary/diary_bloc.dart';
 import '../feature/diary/presentation/viewmodels/new_diary/new_diary_bloc.dart';
 import '../feature/setting/presentation/screens/alarm_setting_bloc_widget.dart';
+import '../feature/setting/presentation/screens/profile_bloc_widget.dart';
 import '../feature/trip/domain/entities/trip_entity.dart';
 import '../feature/trip/presentation/screens/edit_trip_bloc_widget.dart';
 import '../feature/trip/presentation/screens/trip_shell_scaffold.dart';
@@ -138,6 +139,10 @@ class AppRouter {
       GoRoute(
         path: '/alarmSetting',
         builder: (context, state) => const AlarmSettingBlocWidget(),
+      ),
+      GoRoute(
+        path: '/profile',
+        builder: (context, state) => const ProfileBlocWidget(),
       ),
       // Trip ShellRoute
       ShellRoute(
