713b917709d2f4960418f368024d7df71b053b2b
Shiny Lee <shinish512@gmail.com>
2025-12-12 22:00:21 +0900

    수정

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lottie/lottie.dart';
+
+import 'feature/auth/presentation/viewmodel/auth_profile/auth_profile_bloc.dart';
+import 'feature/auth/presentation/viewmodel/auth_profile/auth_profile_state.dart';
 
 // [이재은] 스플래시 로띠 화면
 // 4초 길이
@@ -23,7 +27,13 @@ class _SplashScreenState extends State<SplashScreen> {
   Future<void> _initialize() async {
     await Future.delayed(const Duration(seconds: 5));
     if (mounted) {
-      context.push('/');
+      final authState = context.read<AuthProfileBloc>().state;
+
+      if (authState is AuthProfileAuthenticated) {
+        context.go('/home');
+      } else {
+        context.go('/login');
+      }
     }
   }
 
