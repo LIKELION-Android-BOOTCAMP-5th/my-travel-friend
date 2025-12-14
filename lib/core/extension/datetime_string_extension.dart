1d9807cf2d707f225e491246fbcdcbdf688c70da
Shiny Lee <shinish512@gmail.com>
2025-12-14 12:29:31 +0900

    날짜 및 시간 현지화 완료

--- lib/core/extension/datetime_string_extension.dart
+++ lib/core/extension/datetime_string_extension.dart
@@ -3,13 +3,18 @@
 extension DateTimeStringExtension on String {
   // yyyy-MM-dd만 남기기
   String toDateOnly() {
-    return split('T').first;
+    try {
+      final dt = DateTime.parse(this).toLocal();
+      return '${dt.year}-${dt.month.toString().padLeft(2, '0')}-${dt.day.toString().padLeft(2, '0')}';
+    } catch (e) {
+      return split('T').first;
+    }
   }
 
   // 시간만 남기기
   String toTimeOnly() {
     try {
-      final dt = DateTime.parse(this);
+      final dt = DateTime.parse(this).toLocal();
       final h = dt.hour.toString().padLeft(2, '0');
       final m = dt.minute.toString().padLeft(2, '0');
       return '$h:$m';
@@ -20,6 +25,12 @@ extension DateTimeStringExtension on String {
 
   // 같은 날인지 비교
   bool isSameDayAs(String other) {
-    return toDateOnly() == other.toDateOnly();
+    try {
+      final d1 = DateTime.parse(this).toLocal();
+      final d2 = DateTime.parse(other).toLocal();
+      return d1.year == d2.year && d1.month == d2.month && d1.day == d2.day;
+    } catch (e) {
+      return toDateOnly() == other.toDateOnly();
+    }
   }
 }
